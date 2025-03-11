658ed74f4405b27f1831d21c6eed9d57438f4e6f
Sanjaykanwasi <sanjaykanwasi3@gmail.com>
2025-03-11 15:30:26 +0530

    f1

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 function connectToDb() {
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.DB_CONNECT)
     .then(() => {
       console.log("Sucessfully Connected to Database");
     })
